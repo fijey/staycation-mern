794a280aa5b19cec5a01dc7bef0e3a4281e277f0
fajar m <fajarm@orderonline.id>
2024-12-09 13:02:04 +0700

    add animation

--- src/parts/Categories.js
+++ src/parts/Categories.js
@@ -1,53 +1,58 @@
 import React from 'react';
 import Button from 'elements/Button';
+import { Fade } from 'react-reveal';
 
 export default function Categories({ data }) {
-  return data.map((category, index1) => {
-    return (
-        <section className='container' key={index1}>
-            <h4 className='mb-3 font-weight-medium'>
-                {category.name}
-            </h4>
-            <div className='container-grid'>
-                {
-                    category.items.length === 0 ? 
-                    <div className='row'>
-                        <div className='col-auto align-items-center'>
-                            There is no property at this category
-                        </div>
-                    </div>
-                    : 
-                    category.items.map((item, index2) => {
-                        return (
-                            <div className='item column-3 row-1' key={`category-${index1}-item${index2}`}>
-                                <div className='card'>
-                                    {
-                                        item.isPopular &&
-                                         <div className='tag'> 
-                                            Popular <span className='font-weight-light'>Choice</span>
-                                        </div>
-                                    }
-                                    <figure className='img-wrapper' style={{height: 180}}>
-                                        <img src={item.imageUrl} alt={item.name} className='img-cover'>
-                                        </img>
-                                    </figure>
-                                    <div className='meta-wrapper'>
-                                        <Button type='link' href={`/properties/${item._id}`} className='streched-link d-block text-gray-500'>
-                                            <h4 className='mb-2 font-weight-medium text-dark'>
-                                                {item.name}
-                                            </h4>
-                                        </Button>
-                                        <span className='text-gray-500'>
-                                            {item.city}, {item.country}
-                                        </span>
+    return data.map((category, index1) => {
+        return (
+            <section className='container' key={index1}>
+                <Fade bottom>
+                    <h4 className='mb-3 font-weight-medium'>
+                        {category.name}
+                    </h4>
+                    <div className='container-grid'>
+                        {
+                            category.items.length === 0 ?
+                                <div className='row'>
+                                    <div className='col-auto align-items-center'>
+                                        There is no property at this category
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </section>
-    )
-  })
+                                :
+                                category.items.map((item, index2) => {
+                                    return (
+                                        <Fade bottom delay={300 * index2}>
+                                            <div className='item column-3 row-1' key={`category-${index1}-item${index2}`}>
+                                                <div className='card'>
+                                                    {
+                                                        item.isPopular &&
+                                                        <div className='tag'>
+                                                            Popular <span className='font-weight-light'>Choice</span>
+                                                        </div>
+                                                    }
+                                                    <figure className='img-wrapper' style={{ height: 180 }}>
+                                                        <img src={item.imageUrl} alt={item.name} className='img-cover'>
+                                                        </img>
+                                                    </figure>
+                                                    <div className='meta-wrapper'>
+                                                        <Button type='link' href={`/properties/${item._id}`} className='streched-link d-block text-gray-500'>
+                                                            <h4 className='mb-2 font-weight-medium text-dark'>
+                                                                {item.name}
+                                                            </h4>
+                                                        </Button>
+                                                        <span className='text-gray-500'>
+                                                            {item.city}, {item.country}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </Fade>
+                                    )
+                                })
+                        }
+                </div>
+            </Fade>
+            </section >
+        )
+})
 }
--- src/parts/header.js
+++ src/parts/header.js
@@ -4,6 +4,7 @@ import Button from 'elements/Button';
 import BrandIcon from 'parts/IconText';
 
 import { useLocation } from 'react-router-dom';
+import { Fade } from 'react-reveal';
 
 
 export default function Header(props) {
@@ -12,28 +13,30 @@ export default function Header(props) {
 		return location?.pathname === path ? " active" : ""
 	}
 	return (
-		<header className="spacing-sm">
-			<div className="container">
-				<nav className="navbar navbar-expand-lg navbar-light">
-					<BrandIcon />
-					<div className="collapse navbar-collapse">
-						<ul className="navbar-nav ml-auto">
-							<li className={`nav-item${getNavLinkClass("/")}`}>
-								<Button className="nav-link" type="link" href="">Home</Button>
-							</li>
-							<li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-								<Button className="nav-link" type="link" href="/browse-by">Browse By</Button>
-							</li>
-							<li className={`nav-item${getNavLinkClass("/stories")}`}>
-								<Button className="nav-link" type="link" href="/stories">Stories</Button>
-							</li>
-							<li className={`nav-item${getNavLinkClass("/agents")}`}>
-								<Button className="nav-link" type="link" href="/agents">Agents</Button>
-							</li>
-						</ul>
-					</div>
-				</nav>
-			</div>
-		</header>
+		<Fade>
+			<header className="spacing-sm">
+				<div className="container">
+					<nav className="navbar navbar-expand-lg navbar-light">
+						<BrandIcon />
+						<div className="collapse navbar-collapse">
+							<ul className="navbar-nav ml-auto">
+								<li className={`nav-item${getNavLinkClass("/")}`}>
+									<Button className="nav-link" type="link" href="">Home</Button>
+								</li>
+								<li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+									<Button className="nav-link" type="link" href="/browse-by">Browse By</Button>
+								</li>
+								<li className={`nav-item${getNavLinkClass("/stories")}`}>
+									<Button className="nav-link" type="link" href="/stories">Stories</Button>
+								</li>
+								<li className={`nav-item${getNavLinkClass("/agents")}`}>
+									<Button className="nav-link" type="link" href="/agents">Agents</Button>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</header>
+		</Fade>
 	);
 }
