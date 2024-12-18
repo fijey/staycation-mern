68587f266651854742cf49e0f990eb35d984b471
fijey <fajarmukti9@gmail.com>
2024-12-18 11:53:31 +0700

    page detail description

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -2,6 +2,9 @@ import Header from 'parts/Header'
 import PageDetailTitle from 'parts/PageDetailTitle'
 import React, { Component } from 'react'
 import itemDetails from 'json/itemDetails.json'
+import FeaturedImage from 'parts/FeaturedImage';
+import PageDetailDescription from 'parts/PageDetailDescription';
+import { Fade } from 'react-reveal';
 
 export default class DetailPage extends Component {
     componentDidMount() {
@@ -17,6 +20,25 @@ export default class DetailPage extends Component {
         <>
             <Header {...this.props}></Header>
             <PageDetailTitle breadcrumb={breadcrumb} data={itemDetails}></PageDetailTitle>
+            <FeaturedImage data={itemDetails.imageUrls}></FeaturedImage>
+
+            <section className="container">
+                <div className="row">
+                    <div className="col-7 pr-5">
+                    <Fade bottom>
+                        <PageDetailDescription data={itemDetails}></PageDetailDescription>
+                    </Fade>
+                    </div>
+                    <div className="col-5">
+                    <Fade bottom>
+                        {/* <BookingForm
+                        itemDetails={page[match.params.id]}
+                        startBooking={this.props.checkoutBooking}
+                        /> */}
+                    </Fade>
+                    </div>
+                </div>
+            </section>
         </>
     )
   }
